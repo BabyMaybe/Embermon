0d1f7b4bf971ea25d27b985cd3d9a93e9ca01c1d
Eric Varela <evarela@mosaiclearning.com>
2019-03-09 01:39:18 -0500

    Added Route Files

---
+++ app/routes/moves/detail.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model(params) {
+    console.log('params');
+    console.log(params);
+    return this.store.findRecord('move', params.move_id)
+  }
+});
