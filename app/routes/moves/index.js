dd222cec1c847f4c0a859d50bb0bd1774ef8ed12
Eric Varela <evarela@mosaiclearning.com>
2019-03-11 03:08:38 -0400

    Moved Moves to index

--- app/routes/moves/index.js
+++ app/routes/moves/index.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  queryParams: {
+    offset: {
+      refreshModel: true
+    },
+    limit: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    console.log('modelParams');
+    console.log(params);
+    return this.store.findAll('move', params);
+    }
 });
