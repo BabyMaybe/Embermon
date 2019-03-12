9a5458ffd6a8912ef5032f380f73730a7d03cb13
Eric Varela <eric_varela@mac.com>
2019-03-12 02:29:11 -0400

    Restructured Adapter to improve normalization handling

--- app/serializers/move.js
+++ app/serializers/move.js
@@ -1,58 +1,62 @@
-import DS from 'ember-data';
+import DS from "ember-data";
 
 export default DS.JSONSerializer.extend({
-
-
   normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
-    let newPayload = {
-      accuracy : payload.accuracy,
-      ailment : payload.meta.ailment.name,
-      category : payload.meta.category.name,
-      critRate : payload.meta.crit_rate,
-      damageClass : payload.damage_class.name,
-      drain : payload.meta.drain,
-      effectChance : payload.effect_chance,
-      effectChanges : payload.effect_changes,
-      effectEntry : payload.effect_entries[0].effect,
-      flavorText : payload.flavor_text_entries.filter(m => m.language.name === 'en').filter(m => ['red-blue', 'yellow', 'firered-leafgreen'].includes(m.version_group.name)).find(m => m.flavor_text).flavor_text.replace(/\n|\r/g, ' ').trim(),
-      flinchChance : payload.meta.flinch_chance,
-      generation : payload.generation.name,
-      healing : payload.meta.healing,
-      id: payload.id,
-      maxHits : payload.meta.max_hits,
-      maxTurns : payload.meta.max_turns,
-      minHits : payload.meta.min_hits,
-      minTurns : payload.meta.min_turns,
-      moveType : payload.type.name,
-      name : payload.name,
-      power : payload.power,
-      pp : payload.pp,
-      priority : payload.priority,
-      statChanges : payload.stat_changes,
-      statChance : payload.meta.stat_chance,
-      target : payload.target.name
-    };
+    // console.log("normalizing single response");
+    // console.log(payload);
 
-    return this._super(store, primaryModelClass, newPayload, id, requestType);
-
+    // payload = payload.results;
+    payload.pokemeta = payload.meta;
+    return this._super(store, primaryModelClass, payload, id, requestType);
   },
 
-  // normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-  //   if (requestType === 'findRecord') {
-  //     const data = this.normalize(primaryModelClass, payload);
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    // console.log("normalizing array response");
+    // console.log(payload);
 
-  //   // console.log('data');
-  //   // console.log(data);
-  //     store.push(data);
-  //     return data;
-  //   } else {
-  //     return payload.results.reduce((acc, result) => {
-  //       let { data } = this.normalize(primaryModelClass, result);
-  //       acc.data.push(data);
-  //       store.push(data);
-  //       return acc;
-  //     }, { data: [] })
-  //   }
-  // },
+    payload.forEach(item => (item.pokemeta = item.meta));
+    return this._super(store, primaryModelClass, payload, id, requestType);
+  },
 
+  normalize(modelClass, resourceHash) {
+    // console.log("doing the normalize");
+    // console.log(resourceHash);
+    resourceHash.accuracy = resourceHash.accuracy;
+    resourceHash.ailment = resourceHash.pokemeta.ailment.name;
+    resourceHash.category = resourceHash.pokemeta.category.name;
+    resourceHash.critRate = resourceHash.pokemeta.crit_rate;
+    resourceHash.damageClass = resourceHash.damage_class.name;
+    resourceHash.drain = resourceHash.pokemeta.drain;
+    resourceHash.effectChance = resourceHash.effect_chance;
+    resourceHash.effectChanges = resourceHash.effect_changes;
+    resourceHash.effectEntry = resourceHash.effect_entries[0].effect;
+    resourceHash.flavorText = resourceHash.flavor_text_entries
+      .filter(m => m.language.name === "en")
+      .filter(m =>
+        ["red-blue", "yellow", "firered-leafgreen"].includes(
+          m.version_group.name
+        )
+      )
+      .find(m => m.flavor_text)
+      .flavor_text.replace(/\n|\r/g, " ")
+      .trim();
+    resourceHash.flinchChance = resourceHash.pokemeta.flinch_chance;
+    resourceHash.generation = resourceHash.generation.name;
+    resourceHash.healing = resourceHash.pokemeta.healing;
+    resourceHash.id = resourceHash.id;
+    resourceHash.maxHits = resourceHash.pokemeta.max_hits;
+    resourceHash.maxTurns = resourceHash.pokemeta.max_turns;
+    resourceHash.minHits = resourceHash.pokemeta.min_hits;
+    resourceHash.minTurns = resourceHash.pokemeta.min_turns;
+    resourceHash.moveType = resourceHash.type.name;
+    resourceHash.name = resourceHash.name;
+    resourceHash.power = resourceHash.power;
+    resourceHash.pp = resourceHash.pp;
+    resourceHash.priority = resourceHash.priority;
+    resourceHash.statChanges = resourceHash.stat_changes;
+    resourceHash.statChance = resourceHash.pokemeta.stat_chance;
+    resourceHash.target = resourceHash.target.name;
+
+    return this._super(modelClass, resourceHash);
+  }
 });
