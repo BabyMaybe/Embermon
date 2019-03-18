239642c579009634278ddad679788d83476901a4
BabyMaybe <eric_varela@mac.com>
2019-03-18 00:33:03 -0400

    Added Basic Pagination to Moves Index

--- app/controllers/moves/index.js
+++ app/controllers/moves/index.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   queryParams: ['offset', 'limit'],
   offset: null,
   limit: null,
+  pagination: service(),
 
   actions: {
     test(move) {
